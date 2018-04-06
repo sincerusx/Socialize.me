dfba7a7ab1712ccc594d398b8abbf671a15314d4
Liam Nelson <lmjnelson@yahoo.com>
2018-04-06 01:43:31 +0100

    Change User Model attribute from "name" to "username"

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,36 +5,6 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-/**
- * App\Models\User
- *
- * @property int $id user identifier
- * @property string $username account username
- * @property string $password bcrypt hashed password
- * @property string $email account email address
- * @property string|null $mobile users mobile number
- * @property int $activated status of account. 0: not activated, 1:activated
- * @property int $verifiedEmail indicates whether email address has been verified
- * @property int $verifiedMobile indicates whether mobile number has been verified
- * @property int $isReal indicates test accounts. 0: test, 1: real
- * @property string|null $rememberToken
- * @property \Carbon\Carbon|null $createdAt
- * @property \Carbon\Carbon|null $updatedAt
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereActivated($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereIsReal($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereMobile($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereUsername($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereVerifiedEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereVerifiedMobile($value)
- * @mixin \Eloquent
- */
 class User extends Authenticatable
 {
     use Notifiable;
@@ -45,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'email', 'password',
     ];
 
     /**
@@ -56,4 +26,22 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
 }
